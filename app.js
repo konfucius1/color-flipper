de43532085af41b6e7d7c66002fcd37c831ed727
Thanakon Kimsan <72403268+konfucius1@users.noreply.github.com>
2023-03-12 03:46:40 +1100

    generate random colors

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
-btn.addEventListener("click", function () {
-  // get random number between 0-3
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+btn.addEventListener('click', function () {
+  const randomColor = getRandomColor();
+
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
 });
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
